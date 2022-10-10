caf04da69f92f45d3eb4f8e50d001c95ebbda0dc
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2022-10-10 14:36:01 +0530

    Added Order Controller & Service

---
+++ src/MVC/MVC.Boilerplate.MVC/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Boilerplate.Service;
+
+namespace MVC.Boilerplate.Controllers
+{
+    public class OrderController : Controller
+    {
+        public async Task<IActionResult> GetOrders()
+        {
+            var result = await OrderService.GetKeyList();
+            return View(result);
+        }
+    }
+}
