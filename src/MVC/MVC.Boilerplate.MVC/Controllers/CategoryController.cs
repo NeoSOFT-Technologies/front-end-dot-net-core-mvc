2d0215665e187c7063450f237a90bef696cb1813
NeoShivam <gupta.shivam@neosoftmail.com>
2022-10-13 13:26:30 +0530

    Added Category Pages

--- src/MVC/MVC.Boilerplate.MVC/Controllers/CategoryController.cs
+++ src/MVC/MVC.Boilerplate.MVC/Controllers/CategoryController.cs
@@ -1,25 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using MVC.Boilerplate.Interfaces;
 using MVC.Boilerplate.Models.Category.Commands;
 
 namespace MVC.Boilerplate.Controllers
 {
+    [Route("Category")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
+        private readonly ILogger<CategoryController> _logger;
+        private readonly INotyfService _notyf;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, ILogger<CategoryController> logger, INotyfService notyf)
         {
             _categoryService = categoryService;
+            _logger = logger;
+            _notyf = notyf;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var d = await _categoryService.GetAllCategories();
-            var dwithevents = await _categoryService.GetAllCategoriesWithEvents(true);
-            var create = await _categoryService.CreateCategory(new CreateCategory { Name = "Test 1" });
+            _logger.LogInformation("Index Action initiated");
+            var response = await _categoryService.GetAllCategories();
+            _logger.LogInformation("Index Action completed");
+            return View(response);
+        }
+
+        [HttpGet("CategoriesWithEvents")]
+        public async Task<IActionResult> CategoriesWithEvents(bool includeHistory)
+        {
+            _logger.LogInformation("CategoriesWithEvents Action initiated");
+            var response = await _categoryService.GetAllCategoriesWithEvents(includeHistory);
+            _logger.LogInformation("CategoriesWithEvents Action initiated");
+            return View(response);
+        }
+
+        [HttpGet("CreateCategory")]
+        public IActionResult CreateCategory()   
+        {
             return View();
+        }
+
+        [HttpPost("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CreateCategory request)
+        {
+            _logger.LogInformation("CreateCategory Action initiated");
+            var response = await _categoryService.CreateCategory(request);
+            _notyf.Success("Category Created Successfully");
+            _logger.LogInformation("CreateCategory Action initiated");
+            return RedirectToAction("Index");
         }
     }
 }
