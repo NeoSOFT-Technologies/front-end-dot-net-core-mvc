4c63f16b81072e1327e1f0c51fa70dc855c81e5d
avivekm <vivek.maurya@neosoftmail.com>
2022-10-13 19:05:42 +0530

    Evevnt delete and update

---
+++ src/MVC/MVC.Boilerplate.MVC/Models/Event/Commands/UpdateEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC.Boilerplate.Models.Event.Commands
+{
+    public class UpdateEvent
+    {
+        public Guid EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Price { get; set; }
+        public string Artist { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
